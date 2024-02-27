d32fae02d63ca44a577339c9301007f0e529376d
thevu29 <vnguyen132az@gmail.com>
2024-02-27 16:15:19 +0700

    search header

--- src/view/content.php
+++ src/view/content.php
@@ -1,15 +1,6 @@
 <div class="content">
-    <div class="content-container">
-        <div class="content-container-search">
-            <form action="/search" class="search w-form" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
-                <div class="search-content"><input class="search-input w-input" maxlength="256" name="query" placeholder="Search for city, neighorhood, agent, address..." type="search" id="search" required="">
-                    <div class="search-button"><input type="submit" class="button button-accent button-fullwidth w-button" value="Search"></div>
-                </div>
-            </form>
-        </div>
-    </div>
     <div class="content-banner">
-        <img src="../assets/images/banner-1.jpg" sizes="(max-width: 1279px) 100vw, 99vw" alt="" class="contetnt-banner-image">
+        <img src="src\assets\images\banner-1.jpg" sizes="(max-width: 1279px) 100vw, 99vw" alt="" class="contetnt-banner-image">
         <div class="hero-background-gradient"></div>
     </div>
-</dov>
+</div>
--- src/view/header.php
+++ src/view/header.php
@@ -28,11 +28,19 @@
                 </ul>
             </div>
             <div class="search-box">
-                <a href="index.php?tim-kiem" class="search-btn">
-                    <span class="material-symbols-outlined">
-                        search
-                    </span>
-                </a>
+                <form action="/tim-kiem" class="search-form" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+                    <div class="search-container">
+                        <input class="search-input" maxlength="256" name="query" placeholder="Nhập sản phẩm" type="search" id="search" required="">
+                        <div class="btn-search-box">
+                            <label class="btn btn-search-label" for="search">
+                                <span class="material-symbols-outlined">
+                                    search
+                                </span>
+                            </label>
+                            <input type="submit" class="btn btn-search" name="search" hidden>
+                        </div>
+                    </div>
+                </form>
                 <a href="index.php?gio-hang" class="cart-btn">
                     <span class="material-symbols-outlined">
                         shopping_bag
