80325d3c0bc4835191a55983defed5f9054559a1
thevu29 <vnguyen132az@gmail.com>
2024-02-27 13:52:45 +0700

    header + login

---
+++ src/view/header.php
@@ -0,0 +1,45 @@
+<header class="header">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light container">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Laptop</a>
+            <div class="collapse navbar-collapse nav-content" id="navbarSupportedContent">
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php?trang-chu">Trang chủ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="search-box">
+                <a href="index.php?tim-kiem" class="search-btn">
+                    <span class="material-symbols-outlined">
+                        search
+                    </span>
+                </a>
+                <a href="index.php?gio-hang" class="cart-btn">
+                    <span class="material-symbols-outlined">
+                        shopping_bag
+                    </span>
+                </a>
+                <a href="index.php?dang-nhap" class="login-btn">
+                    <i class="fa-regular fa-user"></i>
+                </a>
+            </div>
+        </div>
+    </nav>
+</header>
---
+++ src/view/login.php
@@ -0,0 +1,68 @@
+<section class="vh-100">
+    <div class="py-3 h-100">
+        <div class="container login-header">
+            <a href="index.php" class="login-header-link">
+                <h1 class="login-header-logo">Laptop</h1>
+            </a>
+            <h1 class="login-header-title">Đăng nhập</h1>
+        </div>
+        <div class="login-content">
+            <div class="row d-flex align-items-center justify-content-center h-100 login-content">
+                <div class="col-md-8 col-lg-7 col-xl-6">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp" class="img-fluid" alt="Phone image">
+                </div>
+                <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1 py-5">
+                    <form class="login-form">
+                        <div class="form-outline mb-4">
+                            <label class="form-label" for="form1Example13">Số điện thoại</label>
+                            <input type="email" id="form1Example13" class="form-control form-control-lg" />
+                        </div>
+
+                        <div class="form-outline mb-4">
+                            <label class="form-label" for="form1Example23">Mật khẩu</label>
+                            <input type="password" id="form1Example23" class="form-control form-control-lg" />
+                        </div>
+
+                        <div class="d-flex justify-content-end align-items-center mb-4">
+                            <a href="#!">Quên mật khẩu?</a>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary btn-lg btn-block btn-login">Đăng nhập</button>
+
+                        <div class="divider d-flex align-items-center my-4">
+                            <p class="text-center fw-bold mx-3 mb-0 text-muted">Hoặc</p>
+                        </div>
+
+                        <div class="d-flex justify-content-between">
+                            <a class="btn btn-primary btn-lg btn-block flex-fill btn-fb-login" style="background-color: #3b5998" href="#!" role="button">
+                                <i class="fab fa-facebook-f me-2"></i>Facebook
+                            </a>
+                            <a class="btn btn-primary btn-lg btn-block flex-fill btn-gg-login" style="background-color: #55acee" href="#!" role="button">
+                                <i class="fab fa-google me-2"></i>Google
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary login-footer">
+            <div class="text-white mb-3 mb-md-0">
+                Copyright © 2020. All rights reserved.
+            </div>
+
+            <div>
+                <a href="#!" class="text-white me-4">
+                    <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#!" class="text-white me-4">
+                    <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#!" class="text-white me-4">
+                    <i class="fab fa-google"></i>
+                </a>
+                <a href="#!" class="text-white">
+                    <i class="fab fa-linkedin-in"></i>
+                </a>
+            </div>
+        </div>
+</section>
