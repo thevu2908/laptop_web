cfecdb307e601a2015589d4c31205b5b68940ad0
thevu29 <vnguyen132az@gmail.com>
2024-02-28 19:46:01 +0700

    showhide pwd

--- index.php
+++ index.php
@@ -24,7 +24,8 @@
             }
         ?>
     </div>
-    <script src="./js/main.js"></script>
+    
+    <script src="./src/assets/js/main.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
