1910f30619daae4c1ef192a601a7f53999f3d29e
thevu29 <vnguyen132az@gmail.com>
2024-03-05 15:13:04 +0700

    test

--- src/view/content.php
+++ src/view/content.php
@@ -26,7 +26,6 @@
         <img src="src\assets\images\banner-1.jpg" sizes="(max-width: 1279px) 100vw, 99vw" alt="" class="contetnt-banner-image">
         <div class="hero-background-gradient"></div>
     </div>
-</div>
     <div class="content-product container">
         <div class="content-product-title text-center">
             <h2>New Arrivals</h2>
