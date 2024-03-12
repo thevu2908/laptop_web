ccb7d9b23c74db8e79fa31782c9f4a20e82ca163
thevu29 <vnguyen132az@gmail.com>
2024-03-12 07:49:17 +0700

    add phpmailer lib, rename repos to repo

--- admin.php
+++ admin.php
@@ -6,7 +6,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Admin laptop</title>
-  <!--test-->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
@@ -20,51 +19,49 @@
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
   <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
   <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
-  <!--font awesome-->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-  <!--css file-->
-  <link rel="stylesheet" href="./admin/assets/css/style.css" />
+  <link rel="stylesheet" href="./src/admin/assets/css/style.css" />
 </head>
 
 <body>
-  <?php include "./admin/view/navigation.php" ?>
+  <?php include "./src/admin/view/navigation.php" ?>
   <div class="content">
-    <?php include "./admin/view/content.php" ?>
+    <?php include "./src/admin/view/content.php" ?>
     <?php
     if (isset($_GET['controller'])) {
       $tmp = $_GET['controller'];
       switch ($tmp) {
         case "dashboard":
-          include "./admin/view/dashboard.php";
+          include "./src/admin/view/dashboard.php";
           break;
         case "sanpham": {
-            include "./admin/view/SanPham.php";
+            include "./src/admin/view/SanPham.php";
             break;
           }
         case "taikhoan": {
-            include "./admin/view/TaiKhoan.php";
+            include "./src/admin/view/TaiKhoan.php";
             break;
           }
         case "nhanvien": {
-            include "./admin/view/NhanVien.php";
+            include "./src/admin/view/NhanVien.php";
             break;
           }
         case "nhomquyen": {
-            include "./admin/view/NhomQuyen.php";
+            include "./src/admin/view/NhomQuyen.php";
             break;
           }
         case "chucnang": {
-            include "./admin/view/ChucNang.php";
+            include "./src/admin/view/ChucNang.php";
             break;
           }
       }
     } else {
-      include "./admin/view/dashboard.php";
+      include "./src/admin/view/dashboard.php";
     }
     ?>
 
   </div>
-  <script src="./admin/assets/js/main.js"></script>
+  <script src="./src/admin/assets/js/main.js"></script>
 </body>
 
 </html>
