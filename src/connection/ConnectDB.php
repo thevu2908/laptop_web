100a644bbf3fb42e1d8c43ff613d365ad855e800
thevu29 <vnguyen132az@gmail.com>
2024-03-12 15:12:23 +0700

    rename

--- src/connection/ConnectDB.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/../config/config.php';
-
-class ConnectDB {
-    public static function getConnection() {
-        return new PDO("mysql:host=".servername.";dbname=".database.";charset=UTF8", username, password);
-    }
-}   
--- src/model/TaiKhoan/TaiKhoanRepo.php
+++ src/model/TaiKhoan/TaiKhoanRepo.php
@@ -1,18 +1,11 @@
 <?php
 
 include __DIR__ . '/../../connection/ConnectDB.php';
-include 'TaiKhoan.php';
 
 class TaiKhoanRepo {
     public function getData() : array | null {
         try {
-            $conn = ConnectDB::getConnection();
-            $statement = $conn->query('SELECT * FROM taikhoan');
-            $accounts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-            if ($accounts) {
-                return $accounts;
-            }
+            
         } catch (Exception $e) {
             echo 'Error: ' . $e->getMessage() . '<br>';
         }
