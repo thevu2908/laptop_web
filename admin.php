ef0462d1ecf71082147018c540b70d70531e4989
thevu29 <vnguyen132az@gmail.com>
2024-03-13 14:52:38 +0700

    fix admin gui

--- admin.php
+++ admin.php
@@ -8,62 +8,63 @@
   <title>Admin laptop</title>
   <link rel="icon" type="image/x-icon" href="server\src\admin\assets\images\admin-icon.svg">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
   <link href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css" rel="stylesheet">
-  <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
   <link rel="stylesheet" href="server/src/admin/assets/css/style.css" />
 </head>
 
 <body>
-  <?php include "./server/src/admin/view/Taskbar.php" ?>
-  <div class="content">
-    <?php include "./server/src/admin/view/Content.php" ?>
-    <?php
-    if (isset($_GET['controller'])) {
-      $tmp = $_GET['controller'];
-      switch ($tmp) {
-        case "dashboard":
-          include "./server/src/admin/view/DashBoard.php";
-          break;
-        case "sanpham": {
+  <div id="admin-main">
+    <?php include "./server/src/admin/view/Taskbar.php" ?>
+    <div class="content">
+      <?php include "./server/src/admin/view/Content.php" ?>
+      <?php
+      if (isset($_GET['controller'])) {
+        $tmp = $_GET['controller'];
+        switch ($tmp) {
+          case "dashboard":
+            include "./server/src/admin/view/DashBoard.php";
+            break;
+          case "sanpham": {
             include "./server/src/admin/view/SanPham.php";
             break;
           }
-        case "taikhoan": {
+          case "taikhoan": {
             include "./server/src/admin/view/TaiKhoan.php";
             break;
           }
-        case "nhanvien": {
+          case "nhanvien": {
             include "./server/src/admin/view/NhanVien.php";
             break;
           }
-        case "nhomquyen": {
+          case "nhomquyen": {
             include "./server/src/admin/view/NhomQuyen.php";
             break;
           }
-        case "chucnang": {
+          case "chucnang": {
             include "./server/src/admin/view/ChucNang.php";
             break;
           }
+        }
+      } else {
+        include "./server/src/admin/view/DashBoard.php";
       }
-    } else {
-      include "./server/src/admin/view/DashBoard.php";
-    }
-    ?>
+      ?>
+    </div>
   </div>
-
+  
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <script src="https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js"></script>
   <script src="./server/src/admin/assets/js/main.js"></script>
+  <script src="./server/src/admin/assets/js/account.js"></script>
   <script src="./client/pages/NhomQuyen.js"></script>
+  <script src="./client/pages/NhanVien.js"></script>
   <script src="./client/pages/TaiKhoan.js"></script>
 </body>
 
