8f178a0e3fcb2efcacf1d0c10d40bfc5dde9f590
thevu29 <vnguyen132az@gmail.com>
2024-03-14 22:18:58 +0700

    fix handle event

--- server/src/admin/assets/js/main.js
+++ server/src/admin/assets/js/main.js
@@ -55,27 +55,34 @@ window.addEventListener("resize", () => {
 if (window.innerWidth < 768) {
   sideBar.classList.add("hide");
 }
+
 //jQuery
-$(document).ready(function(){
+$(document).ready(function() {
 	// Activate tooltip
 	$('[data-toggle="tooltip"]').tooltip();
 	
 	// Select/Deselect checkboxes
 	var checkbox = $('table tbody input[type="checkbox"]');
-	$("#selectAll").click(function(){
+	$("#selectAll").click(function() {
 		if(this.checked){
-			checkbox.each(function(){
+			checkbox.each(function() {
 				this.checked = true;                        
-			});
+			})
 		} else{
-			checkbox.each(function(){
+			checkbox.each(function() {
 				this.checked = false;                        
 			});
 		} 
-	});
-	checkbox.click(function(){
+	})
+
+	checkbox.click(function() {
 		if(!this.checked){
 			$("#selectAll").prop("checked", false);
 		}
-	});
-});
+	})
+
+  //disable search form submit
+  $(document).on('click', '.btn-search-admin', e => {
+    e.preventDefault()
+  })
+})
