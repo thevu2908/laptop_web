b44de3e782e55afa1ec83048fd098877f392e2c3
thevu29 <vnguyen132az@gmail.com>
2024-03-15 20:08:47 +0700

    config db

---
+++ server/src/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+// database configuration
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'web2');
--- server/src/model/ConnectDB.php
+++ server/src/model/ConnectDB.php
@@ -1,17 +1,13 @@
 <?php
 class ConnectDB {
-    private $servername;
-    private $username;
-    private $password;
-    private $database;
-    public $conn;
+    private $servername = DB_HOST;
+    private $username = DB_USERNAME;
+    private $password = DB_PASSWORD;
+    private $dbName = DB_NAME;
+    protected $conn;
 
     public function __construct() {
-        $this->servername = "localhost";
-        $this->username = "root";
-        $this->password = "";
-        $this->database = "web2";
-        $this->conn = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
+        $this->conn = mysqli_connect($this->servername, $this->username, $this->password, $this->dbName);
 
         if (!$this->conn) {
             die("Connection failed: ". mysqli_connect_error());
