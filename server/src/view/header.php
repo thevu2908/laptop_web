ca74d9c217b357c66a41bb62e05d6ff55ec94699
thevu29 <vnguyen132az@gmail.com>
2024-03-17 15:10:12 +0700

    fix action search

--- server/src/view/header.php
+++ server/src/view/header.php
@@ -4,9 +4,9 @@
             <a class="navbar-brand" href="index.php">
                 <h1 class="header-logo">LAPTOP</h1>
             </a>
-            <form action="/tim-kiem" class="search-form" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
+            <form action="?tim-kiem" class="search-form" method="GET" style="opacity: 1; transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d;">
                 <div class="search-container">
-                    <input class="search-input" maxlength="256" name="query" placeholder="Nhập sản phẩm" type="search" id="search" required="">
+                    <input class="search-input" maxlength="256" name="tim-kiem" placeholder="Nhập sản phẩm" type="search" id="search-input" required="">
                     <div class="btn-search-box">
                         <label class="btn btn-search-label" for="search">
                             <span class="material-symbols-outlined">
