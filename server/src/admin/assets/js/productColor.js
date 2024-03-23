4f4015cca0f8bf3f5ac303cdb04cb17bf0a02404
thevu29 <vnguyen132az@gmail.com>
2024-03-23 10:15:25 +0700

    fix add color form

--- server/src/admin/assets/js/productColor.js
+++ server/src/admin/assets/js/productColor.js
@@ -1,22 +1,14 @@
-const colorHexIdElement = document.querySelector('#product-color-id')
+const colorSelect = document.querySelector('#product-color-select')
 
 const isColor = strColor => {
-    const s = new Option().style;
-    s.color = strColor;
-    return s.color !== '';
+    const s = new Option().style
+    s.color = strColor
+    return s.color !== ''
 }
 
-if (colorHexIdElement) {
-    colorHexIdElement.oninput = e => {
-        const colorHexId = e.target.value
-        const colorDisplay = document.querySelector('.product-color-display')
-
-        if (colorHexId === '#fff') {
-            colorDisplay.classList.add('shadow')
-        } else {
-            colorDisplay.classList.remove('shadow')
-        }
-        
-        colorDisplay.style.backgroundColor = colorHexId
+if (colorSelect) {
+    colorSelect.onchange = e => {
+        const hex = e.target.value
+        document.querySelector('#product-color-id').value = hex
     }
 }
--- server/src/admin/view/MauSac.php
+++ server/src/admin/view/MauSac.php
@@ -9,17 +9,19 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <div class="form-group">
-                        <label for="product-color-id">Mã màu</label>
-                        <input type="text" class="form-control" id="product-color-id" name="product-color-id">
+                    <div class="form-group row">
+                        <div class="col-6 row align-items-center">
+                            <label for="product-color-select" class="mb-0 col-6 pr-0">Chọn màu:</label>
+                            <input type="color" class="form-control col-4" id="product-color-select" name="product-color-select">
+                        </div>
+                        <div class="col-6 row align-items-center">
+                            <label for="product-color-id" class="mb-0 col-4 px-0">Mã màu:</label>
+                            <input type="text" class="form-control col-8" id="product-color-id" name="product-color-id" disabled style="height: 30px;">
+                        </div>
                     </div>
                     <div class="form-group">
                         <label for="product-color-name">Tên màu</label>
                         <input type="text" class="form-control" id="product-color-name" name="product-color-name">
-                    </div>
-                    <div class="form-group d-flex align-items-center">
-                        <span class="mr-2">Màu hiển thị:</span>
-                        <div class="product-color-display" style="width: 30px; height: 30px;"></div>
                     </div>
                 </div>
                 <div class="modal-footer">
