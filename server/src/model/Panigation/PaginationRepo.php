fba7f4b79e74d3f7ff29f5a7b82b797314494373
Văn <95088369+maivantai2003@users.noreply.github.com>
2024-04-03 22:33:44 +0700

    update

--- server/src/model/Panigation/PaginationRepo.php
+++ server/src/model/Panigation/PaginationRepo.php
@@ -7,14 +7,21 @@ class PaginationRepo extends ConnectDB {
                 $query = "SELECT DISTINCT ma_quyen,ma_chuc_nang FROM chitietquyen ORDER BY 1 ASC LIMIT {$start},{$limit}";
             } else if ($table == "sanpham") {
                 $query = "
+                SELECT sp.*, ctsp.ma_ctsp, ten_thuong_hieu, ten_loai, ten_hdh, ten_mau, ten_chip, ten_card, ram, rom 
+                FROM chitietsanpham ctsp
+                JOIN (
                     SELECT sp.*, ten_thuong_hieu, ten_loai, ten_hdh
                     FROM sanpham sp 
                     JOIN thuonghieu th ON sp.ma_thuong_hieu = th.ma_thuong_hieu
                     JOIN theloai tl ON sp.ma_the_loai = tl.ma_the_loai
                     JOIN hedieuhanh hdh ON sp.ma_hdh = hdh.ma_hdh
                     WHERE sp.trang_thai = '0'
-                    ORDER BY sp.ma_sp ASC LIMIT {$start}, {$limit}
-                ";
+                ) AS sp ON ctsp.ma_sp = sp.ma_sp  
+                JOIN mausac ms ON ctsp.ma_mau = ms.ma_mau
+                JOIN chipxuly cxl ON ctsp.ma_chip_xu_ly = cxl.ma_chip_xu_ly
+                JOIN carddohoa cdh ON ctsp.ma_carddohoa = cdh.ma_card
+                ORDER BY 1 ASC LIMIT {$start},{$limit}
+            ";
             } else {
                 $query = "SELECT * from  $table ORDER BY 1 ASC LIMIT {$start},{$limit}";
             }
@@ -31,35 +38,7 @@ class PaginationRepo extends ConnectDB {
             echo 'Error: ' . $e->getMessage() . '<br>';
             return null;
         }
-<<<<<<< HEAD
-        else if ($table == "sanpham") {
-            $query = "
-                SELECT sp.*, ctsp.ma_ctsp, ten_thuong_hieu, ten_loai, ten_hdh, ten_mau, ten_chip, ten_card, ram, rom 
-                FROM chitietsanpham ctsp
-                JOIN (
-                    SELECT sp.*, ten_thuong_hieu, ten_loai, ten_hdh
-                    FROM sanpham sp 
-                    JOIN thuonghieu th ON sp.ma_thuong_hieu = th.ma_thuong_hieu
-                    JOIN theloai tl ON sp.ma_the_loai = tl.ma_the_loai
-                    JOIN hedieuhanh hdh ON sp.ma_hdh = hdh.ma_hdh
-                    WHERE sp.trang_thai = '0'
-                ) AS sp ON ctsp.ma_sp = sp.ma_sp  
-                JOIN mausac ms ON ctsp.ma_mau = ms.ma_mau
-                JOIN chipxuly cxl ON ctsp.ma_chip_xu_ly = cxl.ma_chip_xu_ly
-                JOIN carddohoa cdh ON ctsp.ma_carddohoa = cdh.ma_card
-                ORDER BY 1 ASC LIMIT {$start},{$limit}
-            ";
-        } 
-        else {
-            $query = "SELECT * from  $table ORDER BY 1 ASC LIMIT {$start},{$limit}";
-        }
-        $result = mysqli_query($this->conn, $query);
-        $arrPanigation = array();
-        while ($row = mysqli_fetch_array($result)) {
-            $arrPanigation[] = $row;
-        }
-        return $arrPanigation;
-    }
+}
 
     public function getCount($table) {
         if ($table == "chitietquyen") {
@@ -70,9 +49,9 @@ class PaginationRepo extends ConnectDB {
                 SELECT count(ctsp.ma_ctsp) as num
                 FROM chitietsanpham ctsp
                 JOIN (
-                    SELECT sp.*, ten_thuong_hieu, ten_loai, ten_hdh
-=======
+                    SELECT sp.*, ten_thuong_hieu, ten_loai, ten_hdh)";
     }
+}
 
     public function getCount($table) {
         try {
