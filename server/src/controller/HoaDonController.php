2f7488b03bc918e7166458a24929535b88eadf95
thevu29 <vnguyen132az@gmail.com>
2024-05-09 11:41:47 +0700

    Dashboard render statistic by date and best  seller products

--- server/src/controller/HoaDonController.php
+++ server/src/controller/HoaDonController.php
@@ -37,6 +37,34 @@ class HoaDonController {
             echo $this->hoadonRepo->addHoaDon($hoadon);
         }
     }
+
+    public function getOrderByMonth($month) {
+        $orders = $this->hoadonRepo->getOrderByMonth($month);
+        $revenue = 0;
+        foreach ($orders as $order) {
+            $revenue += $order['thanh_tien'];
+        }
+        echo json_encode([
+            'orders' => $orders,
+            'revenue' => $revenue
+        ]);
+    }
+
+    public function getOrderByDate($date) {
+        $orders = $this->hoadonRepo->getOrderByDate($date);
+        $revenue = 0;
+        foreach ($orders as $order) {
+            $revenue += $order['thanh_tien'];
+        }
+        echo json_encode([
+            'orders' => $orders,
+            'revenue' => $revenue
+        ]);
+    }
+
+    public function getBestSeller($amount) {
+        echo json_encode($this->hoadonRepo->getBestSeller($amount));
+    }
 }
 
 $hoadonctl = new HoaDonController();
@@ -59,6 +87,18 @@ switch ($action) {
         $tinh_trang = $_POST['tinh_trang'];
         $search = $_POST['search'];
         $hoadonctl->getHoaDonByKhachHang($ma_kh, $tinh_trang, $search);
+        break;
+    case 'get-by-date':
+        $date = $_POST['date'];
+        $hoadonctl->getOrderByDate($date);
+        break;
+    case 'get-by-month':
+        $month = $_POST['month'];
+        $hoadonctl->getOrderByMonth($month);
+        break;
+    case 'get-best-seller':
+        $amount = isset($_POST['amount']) ? $_POST['amount'] : 5;
+        $hoadonctl->getBestSeller($amount);
         break;
     case 'add':
         $obj = json_decode(json_encode($_POST['bill']));
@@ -84,7 +124,6 @@ switch ($action) {
 
             $hoadonctl->addHoaDon($hoadon);
         }
-
     default:
         break;
 }
