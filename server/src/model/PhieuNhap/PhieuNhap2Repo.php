9ed0d78395ca6f3804ce4d3a0e9889427a8907bf
thevu29 <vnguyen132az@gmail.com>
2024-05-13 22:49:48 +0700

    fix merge

--- server/src/model/PhieuNhap/PhieuNhap2Repo.php
+++ server/src/model/PhieuNhap/PhieuNhap2Repo.php
@@ -4,10 +4,12 @@ class PhieuNhap2Repo extends ConnectDB {
     public function getData() : array | null {
         $phieunhaps1 = [];
         try {
-            $statement = mysqli_query($this->conn, "SELECT * FROM sanpham sp JOIN chitietsanpham ctsp ON sp.ma_sp = ctsp.ma_sp JOIN mausac ms ON ctsp.ma_mau = ms.ma_mau
-            JOIN chipxuly cxl ON ctsp.ma_chip_xu_ly = cxl.ma_chip_xu_ly
-            JOIN carddohoa cdh ON ctsp.ma_carddohoa = cdh.ma_card
-            ORDER BY sp.ma_sp, ctsp.ma_ctsp");
+            $statement = mysqli_query($this->conn, 
+                "SELECT * FROM sanpham sp JOIN chitietsanpham ctsp ON sp.ma_sp = ctsp.ma_sp JOIN mausac ms ON ctsp.ma_mau = ms.ma_mau
+                JOIN chipxuly cxl ON ctsp.ma_chip_xu_ly = cxl.ma_chip_xu_ly
+                JOIN carddohoa cdh ON ctsp.ma_carddohoa = cdh.ma_card
+                ORDER BY sp.ma_sp, ctsp.ma_ctsp
+            ");
 
             while ($row = mysqli_fetch_array($statement)) {
                 $phieunhaps1[] = $row;
@@ -19,6 +21,4 @@ class PhieuNhap2Repo extends ConnectDB {
         }
         return null;
     }
-
 }
-?>
