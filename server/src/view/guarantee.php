4b72a111911bc6bf53270826ea4a20f4ae133860
Văn <95088369+maivantai2003@users.noreply.github.com>
2024-05-14 09:18:13 +0700

    update

--- server/src/view/guarantee.php
+++ server/src/view/guarantee.php
@@ -3,6 +3,11 @@
 <div class="container">
   <h1 class="mt-5 mb-4 text-center">Tra cứu bảo hành sản phẩm</h1>
   <form>
+  <div class="form-group">
+      <a href="#" id="ThoiGianBaoHanhUser" class="btn btn-success" data-toggle="modal">
+							<span>Tra cứu thời gian bảo hành</span>
+							</a>
+    </div>
     <div class="form-group">
       <label for="serialNumber">Nhập số IMEI(Phía Dưới Đế LapTop):</label>
       <p id="show-err"></p>
@@ -29,4 +34,32 @@
     </table>
   </div>
 </div>
+<div id="tracuuThoiGianBaoHanhUser" class="modal fade">
+<div class="modal-dialog">
+			<div class="modal-content">
+				<form>
+					<div class="modal-header">
+						<h4 class="modal-title">Tra Cứu Thời Hạn Bảo Hành</h4>
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div class="modal-body">
+						<select class="form-control form-control-sm mb-3" aria-label="Default select example" id="admin-select-hinhthuctracuu">
+							<option value="imei" selected>Mã IMEI</option>
+							<option value="sdt">Số Điện Thoại</option>
+						</select>
+						<div class="form-group" id="imei">
+							<label>Số Điện Thoại hoặc IMEI(Phía Dưới Đế LapTop)</label>
+							<input type="text" class="form-control" name="" id="thoigian-imei">
+						</div>
+					</div>
+					<div id="ketqua-tracuu" class="modal-body">
+					</div>
+					<div class="modal-footer">
+						<input type="button" class="btn btn-danger" data-dismiss="modal" id="btnCancelTraCuu" value="Hủy">
+						<input type="button" class="btn btn-info" id="btnTraCuuThoiGian" value="Tra cứu">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 <?php include "./server/src/view/footer.php" ?>
